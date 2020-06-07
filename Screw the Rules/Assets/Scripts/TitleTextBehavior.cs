d1f9a5a33e925df0d37270926c15ca76c1223938
Josh Eslinger <jbeslinger@gmail.com>
2020-06-07 13:35:12 -0500

    Imported Unity's LeanTween package. Animated the title & subtitle.  Also added some randomization to the font + color, including an OnClick method call to such methods. AKA, you can click the title and subtitle to change color + font now.

---
+++ Screw the Rules/Assets/Scripts/TitleTextBehavior.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class TitleTextBehavior : MonoBehaviour
+{
+    #region Fields
+    public List<Font> randomFonts;
+    public List<Color> randomColors;
+    #endregion
+
+    #region Members
+    private Text m_MyText;
+    #endregion
+
+    #region Methods
+    private void Awake()
+    {
+        m_MyText = GetComponent<Text>();
+        ChangeFontAndColor();
+    }
+
+    // This is the public method for the button event OnClick to invoke.
+    // It calls both private methods ChangeFont and ChangeColor.
+    // They do exactly what they say to the text component.
+    public void ChangeFontAndColor()
+    {
+        ChangeFont();
+        ChangeColor();
+    }
+
+    private void ChangeFont()
+    {
+        if (randomFonts.Count == 0)
+        {
+            Debug.LogError("Fill this array with fonts so I can have one to pick from, please.");
+            return;
+        }
+
+        int index = Random.Range(0, randomFonts.Count);
+        m_MyText.font = randomFonts[index];
+    }
+
+    private void ChangeColor()
+    {
+        if (randomFonts.Count == 0)
+        {
+            Debug.LogError("Fill this array with colors so I can have one to pick from, please.");
+            return;
+        }
+
+        int index = Random.Range(0, randomColors.Count);
+        m_MyText.color = randomColors[index];
+    }
+    #endregion
+}
---
+++ Screw the Rules/Assets/Scripts/TitleTextFlyInTween.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleTextFlyInTween : MonoBehaviour
+{
+    #region Fields
+    public float flyInDuration;
+    #endregion
+
+    private void Start()
+    {
+        LeanTween.moveLocalX(gameObject, 0, flyInDuration);
+    }
+}
