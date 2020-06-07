a1477c6325b2903461c34eb31e1d70889c23c216
Josh Eslinger <jbeslinger@gmail.com>
2020-06-07 11:59:35 -0500

    Added functional UI elements for: level number, quit button.  Also added a test backdrop (stolen artwork, so please remove) with animation.

---
+++ Screw the Rules/Assets/Scripts/LevelNumberBehavior.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelNumberBehavior : MonoBehaviour
+{
+    // Simply put, this script, when attached to a Canvas Text object
+    // will grab the Text component and set the text to the scene's buildIndex + 1
+    // OR, to put it another way, the level number!
+    void Awake()
+    {
+        GetComponent<Text>().text = "" + SceneManager.GetActiveScene().buildIndex + 1;
+    }
+}
---
+++ Screw the Rules/Assets/Scripts/QuitButtonBehavior.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitButtonBehavior : MonoBehaviour
+{
+    // This button is also constantly listening for the Escape key
+    // to be pressed so it can quit the game.
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+            QuitGame();
+    }
+
+    // The public method for the button's OnClick event to subscribe to
+    // Quits the game, duh.
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
