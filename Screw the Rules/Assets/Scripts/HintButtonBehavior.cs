06fb0b28be830d801091539d7109c567e75fadea
Josh Eslinger <jbeslinger@gmail.com>
2020-06-07 12:39:17 -0500

    Added working hint button UI element!

---
+++ Screw the Rules/Assets/Scripts/HintButtonBehavior.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HintButtonBehavior : MonoBehaviour
+{
+    public Text hintText;
+
+    // Simply stores a reference to the hintText UI text element and sets it
+    // to enabled when the ShowHintText() is called (by the button's OnClick event).
+    public void ShowHintText()
+    {
+        if (hintText != null)
+            hintText.enabled = true;
+        else
+            Debug.LogError("Assign the hintText variable, you dope!");
+    }
+}
