b797973ec53ba5555d14065f55777f4e77ef0614
Josh Eslinger <jbeslinger@gmail.com>
2020-06-08 17:41:14 -0500

    Added full click and drag functionality to one Tetris piece.  It also invokes a method in the Tetris board to check for matches.

---
+++ Screw the Rules/Assets/Scripts/TetrisCompleteListenerBehavior.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class TetrisCompleteListenerBehavior : MonoBehaviour
+{
+    #region Methods
+    public void CheckBoardForWin()
+    {
+        
+    }
+    #endregion
+}
--- Screw the Rules/Assets/Scripts/TetrisPieceDragBehavior.cs
+++ Screw the Rules/Assets/Scripts/TetrisPieceDragBehavior.cs
@@ -1,18 +1,40 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.Events;
 
 public class TetrisPieceDragBehavior : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    #region Fields
+    public float incrementSnap = 1.0f;
+
+    public UnityEvent onDrop;
+    #endregion
+
+    #region Methods
+    private void Awake()
     {
-        
+        if (onDrop == null)
+            onDrop = new UnityEvent();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnMouseDrag()
     {
-        
+        Vector3 newPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        newPos.z = 0;
+        transform.position = newPos;
     }
+
+    private void OnMouseUp()
+    {
+        float xPos = transform.position.x, yPos = transform.position.y;
+        Vector2 snappedPosition = new Vector2(RoundToNearestMultiple(xPos, incrementSnap), RoundToNearestMultiple(yPos, incrementSnap));
+        transform.position = snappedPosition;
+        onDrop.Invoke();
+    }
+
+    private float RoundToNearestMultiple(float numberToRound, float multipleOf)
+    {
+        int multiple = Mathf.RoundToInt(numberToRound / multipleOf);
+        return multiple * multipleOf;
+    }
+    #endregion
 }
