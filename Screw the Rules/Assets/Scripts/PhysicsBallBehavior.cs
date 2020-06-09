73719217622f4420aa315cd97d3b5c65dedd3d53
Josh Eslinger <jbeslinger@gmail.com>
2020-06-08 21:58:46 -0500

    Final FINAL addition. It bugged me that there wasn't any idicator as to what the ? button did, so I added the word Hint above it.

---
+++ Screw the Rules/Assets/Scripts/PhysicsBallBehavior.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class PhysicsBallBehavior : MonoBehaviour
+{
+    #region Members
+    CompositeCollider2D m_MyCollider;
+    #endregion
+
+    #region Methods
+    private void Awake()
+    {
+        m_MyCollider = GetComponent<CompositeCollider2D>();
+
+        foreach (GameObject ball in GameObject.FindGameObjectsWithTag("Physics Ball"))
+        {
+            Physics2D.IgnoreCollision(m_MyCollider, ball.GetComponent<Collider2D>());
+        }
+    }
+    #endregion
+}
